a8a67289a1d6d5fc691d0fe9b629ed0d3cdeee74
hiarun01 <hiarun02@gmail.com>
2025-10-20 00:40:53 +0530

    dashboard page: add create porject dailog

--- lib/db.ts
+++ lib/db.ts
@@ -2,14 +2,4 @@ import {PrismaClient} from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-const connectDB = async () => {
-  try {
-    await prisma.$connect();
-    console.log("Database connected successfully");
-  } catch (error) {
-    console.error("Database connection failed:", error);
-    prisma.$disconnect();
-  }
-};
-
-export {connectDB, prisma};
+export default prisma;
